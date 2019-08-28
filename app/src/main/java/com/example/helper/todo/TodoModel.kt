19e8b49a5196fe4fc8699a06c9fd0814e338b292
Emmanuel Omona <emmanuel.omona@andela.com>
2019-08-28 15:36:21 +0300

    new commit

--- app/src/main/java/com/example/helper/todo/TodoModel.kt
+++ app/src/main/java/com/example/helper/todo/TodoModel.kt
@@ -1,42 +1,7 @@
-package com.example.helper.todo;
+package com.example.helper.todo
 
-import android.os.Parcelable;
+import android.os.Parcelable
 
-import java.io.Serializable;
+import java.io.Serializable
 
-public class TodoModel implements Serializable {
-
-    private String message;
-    private boolean status;
-    private String time;
-
-    public TodoModel(String message, boolean status, String time) {
-        this.message = message;
-        this.status = status;
-        this.time = time;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public boolean getStatus() {
-        return status;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-}
+class TodoModel(var message: String?, var status: Boolean, var time: String?) : Serializable
